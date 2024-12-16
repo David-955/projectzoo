4c5f65a35bfd94b7a7e8e557e668c16e5deea042
johanis074 <johanis.francoise@lepoles.org>
2024-12-16 14:59:55 +0100

    version 3

---
+++ src/Repository/ZoosRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Zoos;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Zoos>
+ */
+class ZoosRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Zoos::class);
+    }
+
+    //    /**
+    //     * @return Zoos[] Returns an array of Zoos objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('z')
+    //            ->andWhere('z.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('z.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Zoos
+    //    {
+    //        return $this->createQueryBuilder('z')
+    //            ->andWhere('z.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+}
