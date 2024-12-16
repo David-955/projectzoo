0bbc4ed9d1ef019b0996b31ee0910ff30ecbb19c
David-955 <caotien.ngo@lepoles.org>
2024-12-16 16:13:59 +0100

    ji

--- migrations/Version20241216142741.php
+++ migrations/Version20241216142741.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241216141930 extends AbstractMigration
+final class Version20241216142741 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -23,7 +23,8 @@ final class Version20241216141930 extends AbstractMigration
         $this->addSql('CREATE TABLE family (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE mainlands (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE zoos (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE animals ADD family_id INT NOT NULL, ADD mainlands_id INT NOT NULL, ADD zoos_id INT NOT NULL, DROP mainlands, DROP family');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE animals ADD family_id INT NOT NULL, ADD mainlands_id INT NOT NULL, ADD zoos_id INT NOT NULL, DROP mainlands, DROP family, DROP mainland');
         $this->addSql('ALTER TABLE animals ADD CONSTRAINT FK_966C69DDC35E566A FOREIGN KEY (family_id) REFERENCES family (id)');
         $this->addSql('ALTER TABLE animals ADD CONSTRAINT FK_966C69DDF162C9AF FOREIGN KEY (mainlands_id) REFERENCES mainlands (id)');
         $this->addSql('ALTER TABLE animals ADD CONSTRAINT FK_966C69DDD73E1E58 FOREIGN KEY (zoos_id) REFERENCES zoos (id)');
@@ -41,9 +42,10 @@ final class Version20241216141930 extends AbstractMigration
         $this->addSql('DROP TABLE family');
         $this->addSql('DROP TABLE mainlands');
         $this->addSql('DROP TABLE zoos');
+        $this->addSql('DROP TABLE messenger_messages');
         $this->addSql('DROP INDEX IDX_966C69DDC35E566A ON animals');
         $this->addSql('DROP INDEX IDX_966C69DDF162C9AF ON animals');
         $this->addSql('DROP INDEX IDX_966C69DDD73E1E58 ON animals');
-        $this->addSql('ALTER TABLE animals ADD mainlands VARCHAR(255) NOT NULL, ADD family VARCHAR(255) NOT NULL, DROP family_id, DROP mainlands_id, DROP zoos_id');
+        $this->addSql('ALTER TABLE animals ADD mainlands VARCHAR(255) NOT NULL, ADD family VARCHAR(255) NOT NULL, ADD mainland VARCHAR(255) NOT NULL, DROP family_id, DROP mainlands_id, DROP zoos_id');
     }
 }
